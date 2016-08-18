3694e8e43953c36ec321aaecf7979b5e8554478f
rphansen91 <rphansen91@gmail.com>
2016-08-18 02:15:42 -0700

    Challenge Accepted ;)

--- results.js
+++ results.js
@@ -3,7 +3,7 @@ function ShowResults (success) {
 
 	let text = (success === true)?"NICE WORK!":"KEEP AT IT"
 	if (typeof success == "string") { text = success }
-	let message = ["<h1>",text,"</h1>"].join("")
+	let message = ["<h1>","<span class='light'>",text,"</span>","</h1>"].join("")
 
 	if (success === true) {
 		root.innerHTML = message
